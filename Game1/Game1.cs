a97542c943eca8d1f488f58f3c7256763b32c28b
Alex9235 <Prototype9235@mail.ru>
2017-07-07 14:30:14 +0400

    1.0

---
+++ Game1/Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Balls ball1;
+        Balls ball2;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+        }
+
+        
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            var Ballinfo = new InfoBall
+            {
+                Texture = Content.Load<Texture2D>("redball"),
+                scale = 0.08f,
+                HeightTexture = 512,
+                WidthTexture =512,
+            };
+            ball1 = new Balls(Ballinfo)
+            {
+                Position = new Vector2(50, 50),
+            };
+            ball2 = new Balls(Ballinfo)
+            {
+                Position = new Vector2(50, 200),
+            };
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            ball1.Position += new Vector2(0.1f, 0) * gameTime.ElapsedGameTime.Milliseconds;
+            ball2.Position += new Vector2(0.1f, -0.05f) * gameTime.ElapsedGameTime.Milliseconds;
+
+            //ball.Update(gameTime);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+            spriteBatch.Begin();
+            ball1.Draw(spriteBatch);
+            ball2.Draw(spriteBatch);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Game1/oject game/Balls.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Balls : RoundObject
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Origin { get; set; }
+
+
+        public override float Radius
+        {
+            get { return info.WidthTexture * info.scale / 2;}
+        }
+
+        public override Vector2 PositionCenter
+        {
+            get { return Position; }
+        }
+
+
+        //private float speed;
+        InfoBall info;
+        
+        public Balls(InfoBall info)
+        {
+            this.info = info;
+            Origin = new Vector2(info.WidthTexture / 2f, info.WidthTexture / 2f);
+        }
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+        public void Draw(SpriteBatch spriteBatch )
+        {
+            spriteBatch.Draw(info.Texture,Position, null, Color.White, 0f, Origin, info.scale, SpriteEffects.None, 0f);
+        }
+    }
+}
